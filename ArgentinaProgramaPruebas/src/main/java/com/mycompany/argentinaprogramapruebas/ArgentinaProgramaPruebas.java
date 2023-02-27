77294084e8b760057504d05de7ceaa65ebfd48c7
joako <joako@carreralinux.com.ar>
2023-02-27 08:52:39 -0300

    buenas

---
+++ ArgentinaProgramaPruebas/src/main/java/com/mycompany/argentinaprogramapruebas/ArgentinaProgramaPruebas.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.argentinaprogramapruebas;
+
+import java.util.Scanner;
+
+public class ArgentinaProgramaPruebas {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int next = sc.nextInt();
+        System.out.println(next);
+        
+      
+    }
+}
