f2268a0d718b7702ee04be05f1d693f73d12e3f8
joako-ampalio <joako@carreralinux.com.ar>
2023-02-27 11:15:28 -0300

    holis

---
+++ PruebasBORRAR/src/main/java/com/mycompany/pruebasborrar/PruebasBORRAR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebasborrar;
+
+/**
+ *
+ * @author nes
+ */
+public class PruebasBORRAR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
